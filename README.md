2d092caea3bb80306766b8da5f5d4193cc2e7584
Abathur8bit <Abathur8bit@8bitcoder.com>
2026-01-26 11:33:22 -0600

    - Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
+
+
+- Use ArgParser instead
+- Need to be able to set a package name for java
+- Modify pom.xml file version tag
+- revision # should be padded to "00"
+- Support major.minor.patch.build
---
+++ bin/versionutil.dart
@@ -0,0 +1,182 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+
+void main(List<String> args) {
+  ArgParser parser = ArgParser()
+    ..addOption("lang",help:"lang=dart|java|cpp Output language (default: dart)")
+    ..addOption("out",help: "out=<path> Output file path")
+    ..addOption("verfile",help: "path=<version.json> Version file to look at")
+    ..addFlag("help",abbr:"h",help: "Usage help.",negatable: false);
+  // --lang=dart|java|cpp   Output language (default: dart)
+  // --out=<path>           Output file path
+  // --verfile=<path>       Version file to look at
+  // --clean                Remove JSON and generated files
+  // --help                 Show this help
+
+  final argResults = parser.parse(args);
+  if(argResults.wasParsed("help")) {
+    _usage(parser.usage);
+    return;
+  }
+  return;
+  final flags = _parseArgs(args);
+
+  if (flags.containsKey('help')) {
+    _usage(parser.usage);
+    return;
+  }
+
+  return;
+
+  final versionFile = File('version.json');
+  final buildFile = File('version-build.json');
+
+  final lang = flags['lang'] ?? 'dart';
+  final outputPath = flags['out'] ?? _defaultOutputPath(lang);
+
+  final outputFile = File(outputPath);
+
+  if (flags.containsKey('clean')) {
+    _clean(versionFile, buildFile, outputFile);
+    return;
+  }
+
+  if (!versionFile.existsSync()) {
+    stderr.writeln('Creating $versionFile');
+    createVersionFile(versionFile,0,1);
+  }
+  if(!buildFile.existsSync()) {
+    stderr.writeln('Creating $buildFile');
+    createBuildFile(buildFile,1);
+  }
+
+  final versionData =
+  jsonDecode(versionFile.readAsStringSync()) as Map<String, dynamic>;
+  final buildData =
+  jsonDecode(buildFile.readAsStringSync()) as Map<String, dynamic>;
+
+  final int version = versionData['version'];
+  final int revision = versionData['revision'];
+  final int build = buildData['build'];
+
+  final formattedBuild = build.toString().padLeft(4, '0');
+  final versionString = '$version.$revision.$formattedBuild';
+
+  outputFile.createSync(recursive: true);
+  outputFile.writeAsStringSync(_generate(lang, versionString, version, revision, build));
+
+  print('Generated $outputPath');
+  print('VERSION=$versionString');
+
+  // Increment build
+  buildData['build'] = build + 1;
+  buildFile.writeAsStringSync(
+    const JsonEncoder.withIndent('  ').convert(buildData),
+  );
+
+  print('Incremented build to ${build + 1}');
+}
+
+Map<String, String> _parseArgs(List<String> args) {
+  final map = <String, String>{};
+
+  for (final arg in args) {
+    if (arg.startsWith('--')) {
+      final parts = arg.substring(2).split('=');
+      map[parts[0]] = parts.length > 1 ? parts[1] : 'true';
+    }
+  }
+
+  return map;
+}
+
+void _usage(String parserUsage) {
+  print('''
+Version generator
+
+Usage:
+  dart run tool/version_gen.dart [options]
+
+Options:
+$parserUsage
+''');
+}
+
+void createVersionFile(File versionFile, int version, int revision) {
+  Map<String, dynamic> versionData = {"version": version, "revision": revision};
+  versionFile.writeAsStringSync(jsonEncode(versionData).toString());
+}
+
+void createBuildFile(File buildFile, int build) {
+  Map<String, dynamic> versionData = {"build": build};
+  buildFile.writeAsStringSync(jsonEncode(versionData).toString());
+}
+
+
+void _clean(File version, File build, File output) {
+  void deleteIfExists(File f) {
+    if (f.existsSync()) {
+      f.deleteSync();
+      print('Deleted ${f.path}');
+    }
+  }
+
+  deleteIfExists(version);
+  deleteIfExists(build);
+  deleteIfExists(output);
+}
+
+String _generate(String lang, String versionString, int version, int revision, int build) {
+  switch (lang) {
+    case 'dart':
+      return '''
+/// GENERATED FILE - DO NOT EDIT
+const String appVersion = "$versionString";
+const int appVersionNumber = $version;
+const int appRevision = $revision;
+const int appBuild = $build;
+''';
+
+    case 'java':
+      return '''
+// GENERATED FILE - DO NOT EDIT
+public final class Version {
+  private Version() {}
+  public static final String APP_VERSION = "$versionString";
+  public static final int APP_VERSIONNUMBER = $version;
+  public static final int APP_REVISION = $revision;
+  public static final int APP_BUILD = $build;
+}
+''';
+
+    case 'cpp':
+      return '''
+// GENERATED FILE - DO NOT EDIT
+#pragma once
+#define APP_VERSION "$version"
+#define APP_VERSIONNUMBER version;
+#define APP_REVISION revision;
+#define APP_BUILD build;
+''';
+
+    default:
+      stderr.writeln('Unsupported language: $lang');
+      exit(2);
+  }
+}
+
+String _defaultOutputPath(String lang) {
+  switch (lang) {
+    case 'dart':
+      return 'lib/version.dart';
+    case 'java':
+      return 'Version.java';
+    case 'cpp':
+      return 'version.h';
+    default:
+      return 'version.txt';
+  }
+}
+
