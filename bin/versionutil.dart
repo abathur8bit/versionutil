1a5a2da003d0b2f8f5c38187d38f613c179d8e58
abathur8bit <orion7019@gmail.com>
2026-01-27 15:04:34 -0600

    - Now able to include patch number

--- bin/versionutil.dart
+++ bin/versionutil.dart
@@ -6,8 +6,10 @@ import 'package:args/args.dart';
 void main(List<String> args) {
   ArgParser parser = ArgParser()
     ..addOption("lang",help:"lang=dart|java|cpp Output language (default: dart)")
+    ..addOption("package",help: "package=<com.axorion> Package for java file")
     ..addOption("out",help: "out=<path> Output file path")
     ..addOption("verfile",help: "path=<version.json> Version file to look at")
+    ..addFlag("clean",help: "Remove JSON and generated files",negatable: false)
     ..addFlag("help",abbr:"h",help: "Usage help.",negatable: false);
   // --lang=dart|java|cpp   Output language (default: dart)
   // --out=<path>           Output file path
@@ -15,37 +17,29 @@ void main(List<String> args) {
   // --clean                Remove JSON and generated files
   // --help                 Show this help
 
-  final argResults = parser.parse(args);
-  if(argResults.wasParsed("help")) {
+  final flags = parser.parse(args);
+  if(flags.wasParsed("help")) {
     _usage(parser.usage);
     return;
   }
-  return;
-  final flags = _parseArgs(args);
-
-  if (flags.containsKey('help')) {
-    _usage(parser.usage);
-    return;
-  }
-
-  return;
 
   final versionFile = File('version.json');
   final buildFile = File('version-build.json');
 
   final lang = flags['lang'] ?? 'dart';
   final outputPath = flags['out'] ?? _defaultOutputPath(lang);
+  String? package = flags['package'];
 
   final outputFile = File(outputPath);
 
-  if (flags.containsKey('clean')) {
+  if (flags['clean']) {
     _clean(versionFile, buildFile, outputFile);
     return;
   }
 
   if (!versionFile.existsSync()) {
     stderr.writeln('Creating $versionFile');
-    createVersionFile(versionFile,0,1);
+    createVersionFile(versionFile,0,1,0);
   }
   if(!buildFile.existsSync()) {
     stderr.writeln('Creating $buildFile');
@@ -59,13 +53,16 @@ void main(List<String> args) {
 
   final int version = versionData['version'];
   final int revision = versionData['revision'];
+  final int patch = versionData['patch'];
   final int build = buildData['build'];
 
   final formattedBuild = build.toString().padLeft(4, '0');
-  final versionString = '$version.$revision.$formattedBuild';
+  final formattedRevision = revision.toString().padLeft(2,"0");
+  final formattedPatch = patch.toString().padLeft(2,"0");
+  final versionString = '$version.$formattedRevision.$formattedPatch.$formattedBuild';
 
   outputFile.createSync(recursive: true);
-  outputFile.writeAsStringSync(_generate(lang, versionString, version, revision, build));
+  outputFile.writeAsStringSync(_generate(lang, versionString, version, revision, patch, build, package));
 
   print('Generated $outputPath');
   print('VERSION=$versionString');
@@ -77,19 +74,6 @@ void main(List<String> args) {
   );
 
   print('Incremented build to ${build + 1}');
-}
-
-Map<String, String> _parseArgs(List<String> args) {
-  final map = <String, String>{};
-
-  for (final arg in args) {
-    if (arg.startsWith('--')) {
-      final parts = arg.substring(2).split('=');
-      map[parts[0]] = parts.length > 1 ? parts[1] : 'true';
-    }
-  }
-
-  return map;
 }
 
 void _usage(String parserUsage) {
@@ -104,8 +88,8 @@ $parserUsage
 ''');
 }
 
-void createVersionFile(File versionFile, int version, int revision) {
-  Map<String, dynamic> versionData = {"version": version, "revision": revision};
+void createVersionFile(File versionFile, int version, int revision, int patch) {
+  Map<String, dynamic> versionData = {"version": version, "revision": revision, "patch": patch};
   versionFile.writeAsStringSync(jsonEncode(versionData).toString());
 }
 
@@ -128,7 +112,7 @@ void _clean(File version, File build, File output) {
   deleteIfExists(output);
 }
 
-String _generate(String lang, String versionString, int version, int revision, int build) {
+String _generate(String lang, String versionString, int version, int revision, int patch,int build,String? package) {
   switch (lang) {
     case 'dart':
       return '''
@@ -136,17 +120,20 @@ String _generate(String lang, String versionString, int version, int revision, i
 const String appVersion = "$versionString";
 const int appVersionNumber = $version;
 const int appRevision = $revision;
+const int appPatch = $patch;
 const int appBuild = $build;
 ''';
 
     case 'java':
       return '''
+${package == null ? "" : "package $package;\n"}
 // GENERATED FILE - DO NOT EDIT
 public final class Version {
   private Version() {}
   public static final String APP_VERSION = "$versionString";
   public static final int APP_VERSIONNUMBER = $version;
   public static final int APP_REVISION = $revision;
+  public static final int APP_PATCH = $patch;
   public static final int APP_BUILD = $build;
 }
 ''';
@@ -155,10 +142,11 @@ public final class Version {
       return '''
 // GENERATED FILE - DO NOT EDIT
 #pragma once
-#define APP_VERSION "$version"
-#define APP_VERSIONNUMBER version;
-#define APP_REVISION revision;
-#define APP_BUILD build;
+#define APP_VERSION "$versionString"
+#define APP_VERSIONNUMBER $version;
+#define APP_REVISION $revision;
+#define APP_PATCH $patch;
+#define APP_BUILD $build;
 ''';
 
     default:
